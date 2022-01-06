52beec32d299117d7385703005ed91b5de3bed5f
jenny <yunhee.kim@smartfoodnet.com>
2022-01-06 22:28:23 +0900

    [1-two-sum] add main method, refactor

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -6,13 +6,21 @@ class Solution {
         while (idx < len) {
             int toFind = target - nums[idx];
 
-            for (int i = idx+1; i < len; i++) {
+            for (int i = idx + 1; i < len; i++) {
                 if (nums[i] == toFind) {
                     return new int[]{idx, i};
                 }
             }
             idx++;
         }
-        return null;
+        return new int[0];
     }
+
+//    public static void main(String[] args) {
+//        int[] nums = {3, 2, 4};
+//        int target = 6;
+//        Solution s = new Solution();
+//        int[] result = s.twoSum(nums, target);
+//        System.out.println(result[0] + " " + result[1]);
+//    }
 }
